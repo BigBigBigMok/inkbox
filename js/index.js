05dfe2fcaae7fc3c2f36e17d28374087b67c3f0f
BigBigBigWhale <15755028927@163.com>
2019-04-27 22:43:32 +0800

    对于首页进行初步的完善

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Mok on 2019/4/27.
+ */
+!(function(){
+	var menu_btn = document.getElementsByClassName('menu')[0];
+	var class_list = document.getElementsByClassName('mobile-classify')[0];
+	menu_btn.addEventListener('click',function () {
+		// class_list.style.height = '3.6rem';
+		// class_list.style.visibility = 'visible';
+		// menu_btn.style.background = 'url("../images/close.png") no-repeat 0 0';
+	})
+})();
